dd6e5446f1e215249098a15d3eb97020380806d6
MagdalenaBernat <magdalena.bernat@onet.pl>
2023-01-30 19:35:27 +0100

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -3,10 +3,14 @@
 const textInput = document.querySelector("#font-size-control");
 const output = document.querySelector("#text");
 
+window.addEventListener("load", (event) => {
+  textInput.value = "56";
+  textInput.dispatchEvent(new Event('input'));
+});
+
 textInput.addEventListener("input", (event) => {
   output.style.fontSize = `${event.target.value}px`;
 });
-
 
 // Napisz skrypt, który reaguje na zmianę wartości input#font-size-control (zdarzenie input) 
 // i zmienia styl inline span#text aktualizując właściwość font - size.
